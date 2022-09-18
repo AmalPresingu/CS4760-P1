1b9734c259a780febc0d2d7427e334419dfe61b0
Amal Presingu <anpkcd@umsystem.edu>
2022-09-18 01:03:45 -0500

    Command line args work except for ./chain -i [test] condition. Should create output filename test.out instead of output.dat. Also need to finish fork/writing to file and transfer to opsys server for submission.

--- main.c
+++ main.c
@@ -1,3 +1,52 @@
 #include "main.h"
 
+int main(int argc, char **argv)
+{
+	FILE *inFile;
+	FILE *outFile;
+
+	char* inFileName = NULL;
+	char* outFileName = NULL;
+
+	int opt; 
+	while ((opt = getopt(argc, argv, "hi:o:")) != 1)
+	{
+		switch(opt)
+		{
+			case 'h':
+				printf("ADD HELP msg tomorrow");
+				break;
+			case 'i':
+				inFileName = optarg;
+				break;
+			case 'o':
+				outFileName = optarg;
+				break;
+		}
+	}
+
+	if (outFileName == NULL && inFileName == NULL)
+	{
+		inFileName = "input.dat";
+		outFileName = "output.dat";
+	}
+
+	else if (outFileName == NULL)
+	{
+		outFileName = "output.dat";
+	}
+
+	inFile = fopen(inFileName, "r");
+
+	if (inFile == NULL)
+	{
+		perror ("INPUTFILENAME does not exist.");
+		return -1;
+	}
+
+	outFile = fopen(outFileName, "w");
+
+	//pid_t parentProcessID = getpid();
+	//int numOfChildren;
+	
 
