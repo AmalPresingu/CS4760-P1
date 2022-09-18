10005cb4349a96c5114403bc37162c07daa19310
Amal Presingu <anpkcd@umsystem.edu>
2022-09-18 16:20:18 -0500

    Base functionality is complete. Issue remains with printing .out file for specific case. Need to complete final error checks and submit.

--- main.c
+++ main.c
@@ -1,3 +1,6 @@
+//Author: Amal Presingu
+//Date:   9/18/2022
+
 #include "main.h"
 
 int main(int argc, char **argv)
@@ -9,12 +12,16 @@ int main(int argc, char **argv)
 	char* outFileName = NULL;
 
 	int opt; 
-	while ((opt = getopt(argc, argv, "hi:o:")) != 1)
+	while ((opt = getopt(argc, argv, "hi:o:")) != -1)
 	{
 		switch(opt)
 		{
 			case 'h':
-				printf("ADD HELP msg tomorrow");
+				printf("\n-------Command Line Help-------\n\n");
+        		        printf("./chain -h                    Helps with command line arguments.\n");
+	           	        printf("./chain -i [INPUTFILENAME]    Takes in a file name and reverses the intergers into output file.\n");
+           			printf("./chain -o [OUTPUTFILENAME]   If user specifies filename, the output will be printed to that file.\n\n");
+				exit(0);
 				break;
 			case 'i':
 				inFileName = optarg;
@@ -22,13 +29,22 @@ int main(int argc, char **argv)
 			case 'o':
 				outFileName = optarg;
 				break;
+			default:
+				printf("Invalid argument. Please type ./chain -h");
 		}
 	}
-
+	
 	if (outFileName == NULL && inFileName == NULL)
 	{
 		inFileName = "input.dat";
 		outFileName = "output.dat";
+	}
+	
+	//input filename is still going to null condition
+	else if (outFileName == NULL && inFileName != "input.dat")
+	{
+		outFileName = inFileName;
+        	strcat(outFileName,".out");
 	}
 
 	else if (outFileName == NULL)
@@ -46,7 +62,76 @@ int main(int argc, char **argv)
 
 	outFile = fopen(outFileName, "w");
 
-	//pid_t parentProcessID = getpid();
-	//int numOfChildren;
+	pid_t parentProcessID = getpid();
+	int numOfChildren;
+	fscanf(inFile, "%d", &numOfChildren);
 	
+	//storing Child Process IDS & creating children
+	pid_t childProcessID[numOfChildren];
+
+	int i;
+	for (i = 0; i < numOfChildren; i++)
+	{
+		pid_t pid = fork();
+		if (pid == 0)
+		{
+			fprintf(outFile, "%d: ", getpid());
+			int j, size;
+			//printing the same integers if I don't skip line
+			for (j = 0; j < (2 * i + 1); j++)
+			{
+				char* line = NULL;
+				size_t len = 0;
+				ssize_t read = getline(&line, &len, inFile);
+			}
+
+			fscanf(inFile, "%d", &size);
+			int arr[size];
+
+			int r; 
+			for (r = 0; r < size; r++)
+			{
+				int input;
+				fscanf(inFile, "%d", &input);
+				arr[r] = input;
+			}
+
+			int p;
+			for (p = size - 1; p >= 0; p--)
+			{
+				fprintf(outFile, "%d ", arr[p]);
+			}
+			fprintf(outFile, "\n");
+			exit(0);
+		}
+		//using waitpid system call to wait for changed state
+		else if (pid > 0)
+		{
+			childProcessID[i] = pid;
+			int childExitStatus;
+			waitpid(pid, &childExitStatus, 0);
+		}
+		//outlier error case
+		else 
+		{
+			perror("Cannot fork process.\n");
+		}
+	}
+	
+	//formatting output file
+	fprintf(outFile, "All children were: ");
+
+	int v;
+	for (v = 0; v < numOfChildren; v++)
+	{
+		fprintf(outFile, "%d ", childProcessID[v]);
+	}
+
+	fprintf(outFile, "\nParent PID: %d", parentProcessID);
+
+	fclose(inFile);
+	fclose(outFile);
+	return 0;
+}
+
 
--- main.h
+++ main.h
@@ -6,7 +6,9 @@
 #include <sys/types.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <getopt.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 
 
